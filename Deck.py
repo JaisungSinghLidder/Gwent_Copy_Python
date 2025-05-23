b65dd139ed56a5f4e6c83534af120b68b3809377
Jaisung Singh Lidder <161393408+JaisungSinghLidder@users.noreply.github.com>
2025-05-23 03:12:20 -0700

    Update Deck.py
    Added the json logic

--- Deck.py
+++ Deck.py
@@ -1,13 +1,14 @@
 from typing import List
 
 from Card import Card
+import json
 import random
 
 from Card_Space.Card import Card
 
 
 class Deck:
-    def __init__(self, faction_name: str):
+    def __init__(self, faction_name: str) -> object:
         #storing how many cards
         self.cards: List[Card] = []
         self.faction_name = faction_name
@@ -20,6 +21,20 @@ class Deck:
             if card.faction != self.faction_name and card.faction != "neutral":
                 return False
         return True
+
+    #right now loads random cards
+    #should be same faction only
+    def load_deck_from_json(self, json_file, faction, deck_size = 5):
+        with open(json_file, 'r') as f:
+            card_data = json.load(f)
+
+        #that ** unpacks each dictionary into keyword arguments
+        all_cards = [Card(**card) for card in card_data]
+
+        faction_cards = [card for card in all_cards if card.faction == faction.lower().strip() or card.faction == "neutral"]
+        
+        self.cards = random.sample(faction_cards, deck_size)
+
 
     #Limiting deck to 10 cards for now
     def check_deck_size(self) -> bool:
@@ -44,4 +59,6 @@ class Deck:
         else:
             random_card = random.randint(0, len(self.cards) - 1 )
             return self.cards.pop(random_card)
+
+
 
