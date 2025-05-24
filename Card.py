55592f3bbece2e5dac7242414a60df13b916f0f5
Jaisung Singh Lidder <161393408+JaisungSinghLidder@users.noreply.github.com>
2025-05-24 03:37:15 -0700

    Update Card.py
    Got rid of getters and setters and added the modern form of dataclass for this file to make it more readable, also added comparisons as well

--- Card.py
+++ Card.py
@@ -1,38 +1,19 @@
+from dataclasses import dataclass, field
+
+
+#using dataclasses since this is just an information class
+#using this gets rid of some of the boilerplate code
+#Since it already creates comparisons
+#now I am going to base it off strength right now to keep the code easier for my AI
+#it probably won't be the smartest yet, but I can deal with that for now until I find another way to do it.
 class Card:
-    #This is the constructor for the class
-    #ability = None is just a default constructor class
-    def __init__(self, card_name, faction, row, strength, card_type, ability = None):
-        #attributes are set to the parameterized values
-        self.card_name = card_name
-        self.faction = faction
-        self.row = row
-        self.strength = strength
-        self.ability = ability
-        self.card_type = card_type
 
-    #returning the strength value
-    def get_strength(self):
-        return self.strength
-
-    #returning the row
-    def get_row(self):
-        return self.row
-
-    #returning the card type
-    def get_card_type(self):
-        return self.card_type
-
-    #set the strength value
-    def set_strength(self,value):
-        self.strength = value
-
-    #returning the faction
-    def get_faction(self):
-        return self.faction
-
-    #returning the ability
-    def get_ability(self):
-        return self.ability
+    card_name: str = field(compare = False)
+    faction: str = field(compare = False)
+    row: str = field(compare = False)
+    strength: int
+    card_type: str =  field(compare = False)
+    ability: str = field(default=None, compare = False)
 
 
 
