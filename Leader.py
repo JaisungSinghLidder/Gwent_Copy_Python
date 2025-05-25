d085dd301136bf1b7f8f9aa68ec05684d9da215f
Jaisung Singh Lidder <161393408+JaisungSinghLidder@users.noreply.github.com>
2025-05-24 18:20:14 -0700

    Update Leader.py
    Adding dataclass to the leader card class

--- Leader.py
+++ Leader.py
@@ -1,8 +1,12 @@
+from dataclasses import dataclass
+
+from Card_Space.Card import Card
+
+#just holding the data, so we are going to make them dataclass
+@dataclass
 class Leader:
 
-    def __init__(self, leader_ability, faction):
+    def __init__(self, leader_name, leader_ability, faction):
+        self.leader_name = leader_name
         self.leader_ability = leader_ability
         self.faction = faction
-
-
-
