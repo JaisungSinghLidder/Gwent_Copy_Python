ee5e7e7bfb44964ae6f50852fb07352afc00a719
Jaisung Singh Lidder <161393408+JaisungSinghLidder@users.noreply.github.com>
2025-05-26 03:39:53 -0700

    Update main.py
    Adding more to the main function

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
-from Card_Space.Card import Card
 from Card_Space.Deck import Deck
-from Faction_Space.Leader import Leader
-from General_Game_Space.Game import Game
 from General_Game_Space.Player import Player
+from Card_Space.CardLoader import CardLoader
 
 #this main function will run the game and it's loop
 def main():
 
     #intializing the decks for the player
+    #their faction is already intialized when the deck is created
     player_one_deck = Deck("northern realms")
-    player_one_deck.load_deck_from_json("Card_Space/cards.json", "northern realm")
+    player_one_deck.load_deck_from_json("Card_Space/cards.json")
     player_two_deck = Deck("nilfgaardian")
-    player_two_deck.load_deck_from_json("Card_Space/cards.json", "nilfgaardian", )
+    player_two_deck.load_deck_from_json("Card_Space/cards.json" )
+
+    #creating leaders now
+    #will ask player what leader they want
+
+    leader_player_one =  CardLoader.load_leaders_from_json("Card_Space/leader_cards.json")
+    leader_player_two = CardLoader.load_leaders_from_json("Card_Space/leader_cards.json")
+
+    player_one_leader_choice = input("Player one, what leader do you want for your deck")
+
+    player_one_leader_card = None
+    player_two_leader_card = None
+
+    for card in leader_player_one:
+        if card == player_one_leader_choice:
+            player_one_leader_card = card
+
+    for card in leader_player_two:
+        if card == player_two_leader_card:
+            player_two_leader_card = card
 
     #creating the players now
 
-    player_one = Player(player_one_deck, )
+    player_one = Player(player_one_deck, player_one_leader_card, "northern realms", False, "Dave")
+    player_two = Player(player_two_deck, player_two_leader_card, "nilfgaardian", False, "Steve")
+
 
 
 
@@ -24,5 +44,7 @@ def main():
 #over here is the executable
 if __name__ == "__main__":
     main()
+
+
 
 
