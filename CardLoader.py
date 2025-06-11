e6378c5f687e3d6367638306fb5f684dc01ba207
Jaisung Singh Lidder <161393408+JaisungSinghLidder@users.noreply.github.com>
2025-06-11 01:28:45 -0700

    Update CardLoader.py
    Added the db loads

--- CardLoader.py
+++ CardLoader.py
@@ -1,12 +1,18 @@
 import json
+import sqlite3
 from Card_Space.Card import Card
 from Card_Space.Leader import Leader
 import random
+
+from DB.import_leader_cards import leader_cards
+
 
 #this class will be handling the json file loading
 
 class CardLoader:
 
+
+    #OLD JSON IMPLEMENTATION
     @staticmethod
     def load_card_from_json(path:str) -> list[Card]:
         with open(path, "r") as f:
@@ -30,4 +36,57 @@ class CardLoader:
 
         return cards
 
+
+    #NEW SQLlite3 IMPLEMENTATION
+    @staticmethod
+    def load_card_from_db(db_path: str, faction: str, deck_size: int = 20) -> list[Card]:
+        conn = sqlite3.connect(db_path)
+        c = conn.cursor()
+
+        faction = faction.lower().strip()
+
+        c.execute("""
+            SELECT card_name, faction, row, base_strength, current_strength, card_type, ability
+            FROM cards
+            WHERE LOWER(faction) = ? or LOWER(faction = 'neutral'
+            """, (faction,))
+
+        rows = c.fetchall()
+        conn.close()
+
+        if len(rows) < deck_size:
+            raise ValueError(f"Not enough cards to build a deck of size {deck_size}. Found {len(rows)}.")
+
+        selected_rows = random.sample(rows, deck_size)
+
+        cards = [Card(
+            card_name = row[0],
+            faction = row[1],
+            row = row[2],
+            base_strength = row[3],
+            current_strength = row[4],
+            card_type = row[5],
+            ability = row[6]
+        ) for row in selected_rows]
+        
         return cards
+
+    @staticmethod
+    def load_leader_card_from_db(db_path: str) -> list[Leader]:
+        conn = sqlite3.connect(db_path)
+        c = conn.cursor()
+
+        c.execute("SELECT leader_name, leader_ability, faction FROM leader_cards")
+        rows = c.fetchall()
+
+        leader_cards = [Leader(
+            leader_name = row[0],
+            leader_ability = row[1],
+            faction = row[2]
+        ) for row in rows]
+
+        conn.close()
+
+        return leader_cards
+
+
