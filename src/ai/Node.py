008730a2011c2822a5baba80fae21482cdaa3df4
Jaisung <jaisung.lidder@gmail.com>
2025-06-26 23:24:10 -0700

    creating the general node class with abstract methods

---
+++ src/ai/Node.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+import random
+
+class Node(ABC):
+    @abstractmethod
+    def find_children(self):
+        return set()
+
+    @abstractmethod
+    def find_random_child(self):
+        return None
+
+    @abstractmethod
+    def is_terminal(self):
+        return True
+
+    @abstractmethod
+    def reward(self):
+        return 0
+
+    @abstractmethod
+    def __hash__(self):
+        return 123456789
+
+    @abstractmethod
+    def __eq__(self,other):
+        return True
