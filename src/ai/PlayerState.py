e35951032515f8979105dda05b100483b3762c50
Jaisung <jaisung.lidder@gmail.com>
2025-06-27 23:08:15 -0700

    finish the dataclass class

---
+++ src/ai/PlayerState.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass, field
+from typing import List, Dict, Tuple
+from src.cards.Card import Card
+from src.cards.Deck import Deck
+from src.general_game_space.Leader import Leader
+
+
+
+@dataclass(frozen=True)
+class PlayerState:
+
+    hand: Tuple[Card, ...]
+    graveyard: Tuple[Card, ...]
+    lives: int
+    board: Dict[str, Tuple[Card, ...]]
+    turn_order: bool
+    sum: int
+    leader_used: bool
+    leader_card: Leader
+    faction: str
+    passed: bool 
+
+
+
+
+
+
+
+
+
+
+
+
+
