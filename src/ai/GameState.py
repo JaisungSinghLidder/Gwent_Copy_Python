9e55ca4eafc60ca5fa37eb77d609810c455f7313
Jaisung <jaisung.lidder@gmail.com>
2025-06-30 00:52:17 -0700

    added some code and further implementation code

--- src/ai/GameState.py
+++ src/ai/GameState.py
@@ -1,6 +1,7 @@
 from src.ai.PlayerState import PlayerState
 from dataclasses import dataclass, field
 from typing import Set, List
+from src.cards.Card import Card
 
 #a dataclass that should provide a snapshot view of the general game information
 #this will allow the MCTS to evaluate these conditions
@@ -13,9 +14,25 @@ class GameState:
     weather_rows: Set[str]
 
     #this class will handle what is playable in the AI player's hand
-    
-    def is_playable(self):
-        pass
+    #the AI need this class because it needs to be able to discern what it can do so that when it generates the tree
+    #the tree isn't filled with wrong moves
+    #IMPLEMENTATION_NOTE: this class need to be pretty comprehensive and prevent any illegal moves
+
+    def is_playable(self, card: Card) -> bool:
+
+        if card.card_type  == "leader" and self.ai_player_state.leader_used:
+            return False
+
+
+
+
+
+
+        #it passed all the failed checks
+        return True
+
+
+
 
     def get_legal_moves(self) -> list:
         legal_moves = []
