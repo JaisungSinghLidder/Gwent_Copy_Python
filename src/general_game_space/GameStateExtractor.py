07771d8a3ffb29946f261d2df63c2ba413f3edc2
Jaisung <jaisung.lidder@gmail.com>
2025-06-29 18:32:03 -0700

    add the extract game class and the sub function within it

---
+++ src/general_game_space/GameStateExtractor.py
@@ -0,0 +1,51 @@
+import copy
+
+from src.general_game_space.Game import Game
+from src.general_game_space.Player import Player
+from src.ai.GameState import GameState
+from src.ai.PlayerState import PlayerState
+
+
+#this class is going to extract the information from the game class to the GameState class
+
+class GameStateExtractor():
+
+    @staticmethod
+    def extract_game(game : Game) -> GameState:
+
+        #creating both player
+        #afterward I will create a sub function that will reduce this code
+
+        def extract_player_info(player: Player):
+            playerHand = copy.deepcopy(player.hand)
+            playerGraveyard = copy.deepcopy(player.graveyard)
+            playerLives = copy.deepcopy(player.lives)
+            playerBoard = copy.deepcopy(player.board)
+            playerPassed = copy.deepcopy(player.passed)
+            playerSum = copy.deepcopy(player.sum)
+            playerLeaderUsed = copy.deepcopy(player.leader_used)
+            playerLeaderCard = copy.deepcopy(player.leader_card)
+            playerFaction = copy.deepcopy(player.faction)
+            playerAIPlayer = copy.deepcopy(player.ai_player)
+
+            return PlayerState(playerHand, playerGraveyard, playerLives, playerBoard, playerPassed, playerSum, playerLeaderUsed, playerLeaderCard, playerFaction, playerAIPlayer)
+
+        playerOneState = extract_player_info(game.player_one)
+
+        playerTwoState = extract_player_info(game.player_two)
+
+        #now creating part of the game structure
+        gameStateRoundCounter = copy.deepcopy(game.round_counter)
+
+        gameWeatherRow = copy.deepcopy(game.active_weather_effect)
+
+        #here we are going to check whether the player is AI
+
+        if (playerOneState.ai_player):
+            gameSnapShot = GameState(playerOneState, playerTwoState, gameStateRoundCounter, gameWeatherRow)
+        else:
+            gameSnapShot = GameState(playerTwoState, playerOneState, gameStateRoundCounter, gameWeatherRow)
+
+
+        return gameSnapShot
+
