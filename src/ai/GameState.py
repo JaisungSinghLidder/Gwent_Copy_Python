3433c23c2fa4a0543723ce72a5f132ee331b1c69
Jaisung <jaisung.lidder@gmail.com>
2025-07-01 00:18:12 -0700

    using the get_legal_move method and adding some code within it

--- src/ai/GameState.py
+++ src/ai/GameState.py
@@ -1,6 +1,6 @@
 from src.ai.PlayerState import PlayerState
 from dataclasses import dataclass, field
-from typing import Set, List
+from typing import Set, List, Union
 from src.cards.Card import Card
 
 #a dataclass that should provide a snapshot view of the general game information
@@ -16,30 +16,27 @@ class GameState:
     #this class will handle what is playable in the AI player's hand
     #the AI need this class because it needs to be able to discern what it can do so that when it generates the tree
     #the tree isn't filled with wrong moves
-    #IMPLEMENTATION_NOTE: this class need to be pretty comprehensive and prevent any illegal moves
+    #IMPLEMENTATION_NOTE: this method need to be pretty comprehensive and prevent any illegal moves
 
-    def is_playable(self, card: Card) -> bool:
-
-        if card.card_type  == "leader" and self.ai_player_state.leader_used:
-            return False
-
-
-
-
-
-
-        #it passed all the failed checks
-        return True
-
-
-
-
-    def get_legal_moves(self) -> list:
+    def get_legal_moves(self) -> List[Union[Card, str]]:
         legal_moves = []
 
-        pass
+        #this case is for the leader
+        #we are going to append a string because using a leader card is a special case
+        if not self.ai_player_state.leader_used:
+            legal_moves.append("USE_LEADER")
+
+        # running through the cards through the hand
+        for card in self.ai_player_state.hand:
+            #checking the cards here
+            legal_moves.append(card)
 
 
+        #allowing the passing the turn
+        legal_moves.append("PASS")
+
+
+        return legal_moves
 
 
 
