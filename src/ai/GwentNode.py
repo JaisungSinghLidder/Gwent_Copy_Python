a39298c3246641e60fae73d7091a24f820328382
Jaisung <jaisung.lidder@gmail.com>
2025-07-18 23:14:39 -0700

    added some of those functions towards the game_state.is_terminal() class

--- src/ai/GwentNode.py
+++ src/ai/GwentNode.py
@@ -14,18 +14,20 @@ class GwentNode(Node):
         self.visits = 0
         self.total_reward = 0.0
 
-
+    #so this is to find the next legal moves in the position
     def find_children(self):
         pass
 
+    #to randomly pick a legal move child
     def find_random_child(self):
         pass
 
+    #deciding whether it is terminal
     def is_terminal(self):
-        pass
+        return self.game_state.is_terminal()
 
     def reward(self):
-        pass
+        return self.game_state.reward()
 
     def __eq__(self, other):
         pass
