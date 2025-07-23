b3813dcdfb5860d33b05a8dffac628fbfb2eae31
Jaisung <jaisung.lidder@gmail.com>
2025-07-22 23:07:50 -0700

    changed the get_legal_moves so that it is more flexible and both player state cna now use it

--- src/ai/GameState.py
+++ src/ai/GameState.py
@@ -24,59 +24,67 @@ class GameState:
     #isn't filled with wrong moves
     #IMPLEMENTATION_NOTE: this method need to be pretty comprehensive and prevent any illegal moves
     #this will also filter out bad moves that serve no purpose
-    def get_legal_moves(self) -> List[Union[Card, str]]:
+    def get_legal_moves(self, player: PlayerState) -> List[Union[Card, str]]:
+
+        opponent = None
+
+        if player == self.ai_player_state:
+            opponent = self.opponent_state
+        else:
+            opponent = self.ai_player_state
+
         legal_moves = []
 
         #this case is for the leader
         #we are going to append a string because using a leader card is a special case
         # now we need to check for faction and illegal moves for that faction in tow
-        if not self.ai_player_state.leader_used:
+        if not player.leader_used:
 
             #okay now we need to check for behaviour of players
             #might need to create a sub function
 
             #northen realms is going to be a simple weather check
-            if self.ai_player_state.faction == "northern realms":
+            if player.faction == "northern realms":
                 if self.active_weather_effect:
                     legal_moves.append("USE_LEADER")
 
-            elif self.ai_player_state.faction == "nilfgaardian":
-                if self.opponent_state.hand:
+            elif player.faction == "nilfgaardian":
+                if opponent.hand:
                     legal_moves.append("USE_LEADER")
 
             #doubling the spy card by two
-            elif self.ai_player_state.faction == "monsters":
+            elif player.faction == "monsters":
                 #check whether there is a spy to double on the board
                 thereSpy = False
 
                 for row in ["melee", "range", "siege"]:
-                    for card in self.ai_player_state.board[row]:
+                    for card in player.board[row]:
                         if card.ability == "spy":
                             thereSpy = True
 
                     if thereSpy:
                         legal_moves.append("USE_LEADER")
 
-            elif self.ai_player_state.faction == "scoia'tael":
+            elif player.faction == "scoia'tael":
                 # because it's always going to be with a deck
                 # we can just append this rule
 
                 legal_moves.append("USE_LEADER")
 
-            elif self.ai_player_state.faction == "skillege":
+            elif player.faction == "skillege":
 
-                if self.ai_player_state.graveyard:
+                if player.graveyard:
                     legal_moves.append("USE_LEADER")
 
 
 
 
         # running through the cards through the hand
-        for card in self.ai_player_state.hand:
+        for card in player.hand:
 
             #checking here if the medic card can target any graveyard card
             if card.ability == "medic":
-                if not self.ai_player_state.graveyard:
+                if not player.graveyard:
                     continue
             #checking the cards here
             legal_moves.append(card)
@@ -85,22 +93,22 @@ class GameState:
             #what to do here, this here needs to change
             #the logic is weak!
             if card.ability == "horn":
-                #checking whether there are any cards in the ai's board
-                has_any_cards = any(self.ai_player_state.board[row] for row in ["melee", "range", "siege"])
+                #checking whether there are any cards in the player's board
+                has_any_cards = any(player.board[row] for row in ["melee", "range", "siege"])
 
                 #skip this card if there is no card on the board period
                 if not has_any_cards:
                     continue
 
-                if any(self.ai_player_state.board["melee"]):
+                if any(player.board["melee"]):
                     horn_row = "melee"
                     legal_moves.append((card,horn_row))
 
-                if any(self.ai_player_state.board["range"]):
+                if any(player.board["range"]):
                     horn_row = "range"
                     legal_moves.append((card, horn_row))
 
-                if any(self.ai_player_state.board["siege"]):
+                if any(player.board["siege"]):
                     horn_row = "siege"
                     legal_moves.append((card, horn_row))
 
@@ -114,7 +122,7 @@ class GameState:
                 only_heroes = True
 
                 #checking if there is any cards in the opponent state
-                has_any_cards = any(self.opponent_state.board[row] for row in ["melee", "range", "siege"])
+                has_any_cards = any(opponent.board[row] for row in ["melee", "range", "siege"])
 
                 #if the opponent has no cards, then why bother with scorching it?
                 if not has_any_cards:
@@ -122,7 +130,7 @@ class GameState:
 
 
                 for row in ["melee", "range","siege"]:
-                    for opponent_card in self.opponent_state.board[row]:
+                    for opponent_card in opponent.board[row]:
                         if opponent_card.ability != "hero":
                             only_heroes = False
                             break
@@ -529,7 +537,7 @@ class GameState:
     #could later make a rule based one or even a min MCTS for the main MCTS to play against
 
     def get_random_move(self):
-        pass 
+        pass
 
 
 
