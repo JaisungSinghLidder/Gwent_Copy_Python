66d5cdf0c778d3a625dfca46dc5c4177d9c97928
Jaisung <jaisung.lidder@gmail.com>
2025-07-29 19:12:14 -0700

    added clone methods to both of these classes

--- src/ai/PlayerState.py
+++ src/ai/PlayerState.py
@@ -28,6 +28,28 @@ class PlayerState:
     siege_row_horn_effect : bool
     opponent_hand: List[Card] = field(default_factory=list)
 
+    def clone(self):
+        return PlayerState(
+            hand=[card.clone() for card in self.hand],
+            graveyard=[card.clone() for card in self.graveyard],
+            board={row: [card.clone() for card in cards] for row, cards in self.board.items()},
+            passed=self.passed,
+            sum=self.sum,
+            leader_used=self.leader_used,
+            leader = self.leader_card.clone(),
+            faction=self.faction,
+            ai_player = self.ai_player,
+            player_name= self.player_name,
+            melee_row_weather_effect = self.melee_row_weather_effect,
+            range_row_weather_effect= self.siege_row_weather_effect,
+            siege_row_weather_effect= self.siege_row_weather_effect,
+            melee_row_horn_effect= self.melee_row_horn_effect,
+            range_row_horn_effect= self.range_row_horn_effect,
+            siege_row_horn_effect= self.siege_row_horn_effect,
+            opponent_hand = [card.clone() for card in self.opponent_hand]
+        )
+
+
 
 
 
--- src/cards/Card.py
+++ src/cards/Card.py
@@ -23,6 +23,18 @@ class Card:
     card_type: str
     ability: str
 
+    def clone(self):
+        return Card(
+            card_name=self.card_name,
+            faction= self.faction,
+            row = self.row,
+            base_strength= self.base_strength,
+            current_strength= self.current_strength,
+            is_affected_by_weather= self.is_affected_by_weather,
+            is_affected_by_horn= self.is_affected_by_horn,
+            card_type= self.card_type,
+            ability = self.ability 
+        )
 
 
 
