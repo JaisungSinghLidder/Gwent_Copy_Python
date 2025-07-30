f60a3311dee0a28d44e64327e315cfdd4a8e8d1d
Jaisung <jaisung.lidder@gmail.com>
2025-07-30 00:18:14 -0700

    Fixed the clone ()

--- src/general_game_space/Leader.py
+++ src/general_game_space/Leader.py
@@ -6,7 +6,18 @@ from src.cards.Card import Card
 @dataclass
 class Leader:
 
-    def __init__(self, leader_name, leader_ability, faction):
+    def __init__(self, leader_name: str, leader_ability: str, faction: str):
         self.leader_name = leader_name
         self.leader_ability = leader_ability
         self.faction = faction
+
+
+    def clone(self):
+
+        return Leader(
+
+            leader_name = self.leader_name,
+            leader_ability = self.leader_ability,
+            faction= self.faction
+
+        )
